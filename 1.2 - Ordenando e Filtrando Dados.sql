741980ed798e2f069383a72b025cfd09b602301a
Francisco-Douglas <111526469+Francisco-Dougllas@users.noreply.github.com>
2022-09-21 14:36:54 -0300

    Add files via upload

--- 1.2 - Ordenando e Filtrando Dados.sql
+++ 1.2 - Ordenando e Filtrando Dados.sql
@@ -71,3 +71,105 @@ WHERE ColorName = 'black'
 SELECT * FROM DimCustomer
 WHERE BirthDate >= '1970-12-31'
 ORDER BY BirthDate DESC
+
+--EXEMPLO
+SELECT * FROM DimProduct
+
+SELECT * FROM Dimproduct
+WHERE ColorName = 'black'
+
+--Exemplo Selecione a Marca Fabrikam onde a cor seja Black
+SELECT * FROM DimProduct
+WHERE BrandName = 'Fabrikam' AND ColorName = 'Black'
+
+--Exemplo Filtrando com mais de uma condiçao
+--Selecione onde a Marca for Contoso ou a cor igual a Branca
+SELECT * FROM DimProduct
+WHERE BrandName = 'Contoso' OR ColorName = 'White'
+
+--Selecione onde a marca for contoso ou fabrikam
+SELECT * FROm DimProduct
+WHERE BrandName = 'Contoso' OR BrandName = 'Fabrikam' OR BrandName = 'Litware'
+
+--Selecionae todas as tabelas da coluna DimEmployee onde nao tenha Marketing
+SELECT * FROM DimEmployee
+WHERE NOT DepartmentName = 'Marketing'
+
+--EXERCICIOS de Fixacão: AND, OR e NOT
+--1 Selecione todas as linhas da tabela DimEmployee de funcionarios do sexo 
+--feminino e do departamento de Finanças
+SELECT *
+FROM DimEmployee
+WHERE Gender = 'F' AND DepartmentName = 'Finance'
+
+--2 Seleciona todas as linhas da tabela dimproduct de produtos da marca
+--contoso e da cor vermelha que tenha um Unitprice maior ou igua a 100
+SELECT * FROM DimProduct
+WHERE BrandName = 'Contoso' AND ColorName = 'red' AND UnitPrice >= 100
+
+--3 Seleciona todas as linhas da tabela dimproduct com produtos da marca
+--Litware ou da marca Fabrikam ou da cor preta
+SELECT * FROM DimProduct
+WHERE BrandName = 'Litware' 
+	OR BrandName = 'Fabrican' 
+	OR ColorName = 'Black'
+
+--4 Seleciona todas as linhas da tabela DimSalesTerritory onde o continente 
+--e a Europa mas o pais Não e igual a Italia
+SELECT * FROM DimSalesTerritory
+WHERE 
+	SalesTerritoryGroup = 'Europe' AND
+	SalesTerritoryCountry = 'Italy'
+	
+--OBS: Cuidados ao Utilizar o AND em Conjunto com o OR
+--Exemplo: Selecione todas as linhas da tabela DimProduct onde a cor do Produto
+--pode ser igual a Preto ou Vermelho, mas a marca deve ser obrigatoriamente igual a fabrikam
+SELECT * FROM DimProduct
+WHERE 
+	(ColorName = 'black' OR ColorName = 'Red')
+		AND BrandName = 'Fabrikam'
+
+--EXEMPLOS COM A TABELA DIMPRODUCT
+SELECT * FROM DimProduct
+WHERE 
+	ColorName IN ('Silver','Blue','White','Red','Black')
+
+
+SELECT * FROM DimEmployee
+WHERE DepartmentName IN ('Production', 'Marketing', 'Engeneering')
+
+--UTILIZACAO DOM COMANDO LIKE
+SELECT * FROM DimProduct
+WHERE ProductName LIKE '%MP3 Player%'
+
+SELECT * FROM DimProduct
+WHERE ProductDescription LIKE 'Type%'
+
+--COMANDO BETWEEN
+SELECT * FROM DimProduct
+WHERE UnitPrice NOT BETWEEN 50 AND 100
+
+SELECT * FROM DimEmployee
+WHERE HireDate BETWEEN '2000-01-01' AND '2000-12-31'
+
+SELECT * FROM DimCustomer
+WHERE CompanyName IS NOT NULL
+
+SELECT * FROM DimCustomer
+WHERE CompanyName IS NULL
+
+
+
+
+--EXERCICIO-1
+SELECT 
+	TOP(100) *
+FROM 
+	FactSales
+ORDER BY SalesQuantity DESC
+
+--EXERCICIO-2
+SELECT 
+	TOP (10) *
+FROM DimProduct
+ORDER BY UnitPrice DESC
